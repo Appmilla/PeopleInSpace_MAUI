503337e980900e47153af4a9d3d711099d71618d
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-06-16 13:20:43 +0100

    CrewModel no longer Reactive

--- PeopleInSpaceMaui/Models/CrewModel.cs
+++ PeopleInSpaceMaui/Models/CrewModel.cs
@@ -1,6 +1,3 @@
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-
 namespace PeopleInSpaceMaui.Models;
 
 using System;
@@ -8,88 +5,81 @@ using System.Globalization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
-public partial class CrewModel : ReactiveObject
+public partial class CrewModel
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("agency")]
+    public string Agency { get; set; }
+
+    [JsonProperty("image")]
+    public Uri Image { get; set; }
+
+    [JsonProperty("wikipedia")]
+    public Uri Wikipedia { get; set; }
+
+    [JsonProperty("launches")]
+    public string[] Launches { get; set; }
+
+    [JsonProperty("status")]
+    public Status Status { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+}
+
+public enum Status { Active, Inactive, Retired, Unknown };
+
+public partial class CrewModel
+{
+    public static CrewModel[] FromJson(string json) => JsonConvert.DeserializeObject<CrewModel[]>(json, Converter.Settings);
+}
+
+internal static class Converter
+{
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
     {
-        [JsonProperty("name")]
-        [Reactive]
-        public string Name { get; set; }
-
-        [JsonProperty("agency")]
-        [Reactive]
-        public string Agency { get; set; }
-
-        [JsonProperty("image")]
-        [Reactive]
-        public Uri Image { get; set; }
-
-        [JsonProperty("wikipedia")]
-        [Reactive]
-        public Uri Wikipedia { get; set; }
-
-        [JsonProperty("launches")]
-        [Reactive]
-        public string[] Launches { get; set; }
-
-        [JsonProperty("status")]
-        [Reactive]
-        public Status Status { get; set; }
-
-        [JsonProperty("id")]
-        [Reactive]
-        public string Id { get; set; }
-    }
-
-    public enum Status { Active, Inactive, Retired, Unknown };
-
-    public partial class CrewModel
-    {
-        public static CrewModel[] FromJson(string json) => JsonConvert.DeserializeObject<CrewModel[]>(json, Converter.Settings);
-    }
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+        DateParseHandling = DateParseHandling.None,
+        Converters =
         {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-            Converters =
-            {
-                StatusConverter.Singleton,
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
-            },
-        };
-    }
+            StatusConverter.Singleton,
+            new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+        },
+    };
+}
 
-    internal class StatusConverter : JsonConverter
+internal class StatusConverter : JsonConverter
+{
+    public override bool CanConvert(Type t) => t == typeof(Status) || t == typeof(Status?);
+
+    public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
     {
-        public override bool CanConvert(Type t) => t == typeof(Status) || t == typeof(Status?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        if (reader.TokenType == JsonToken.Null) return null;
+        var value = serializer.Deserialize<string>(reader);
+        if (value == "active")
         {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            if (value == "active")
-            {
-                return Status.Active;
-            }
-            throw new Exception("Cannot unmarshal type Status");
+            return Status.Active;
         }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (Status)untypedValue;
-            if (value == Status.Active)
-            {
-                serializer.Serialize(writer, "active");
-                return;
-            }
-            throw new Exception("Cannot marshal type Status");
-        }
-
-        public static readonly StatusConverter Singleton = new StatusConverter();
+        throw new Exception("Cannot unmarshal type Status");
     }
+
+    public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+    {
+        if (untypedValue == null)
+        {
+            serializer.Serialize(writer, null);
+            return;
+        }
+        var value = (Status)untypedValue;
+        if (value == Status.Active)
+        {
+            serializer.Serialize(writer, "active");
+            return;
+        }
+        throw new Exception("Cannot marshal type Status");
+    }
+
+    public static readonly StatusConverter Singleton = new StatusConverter();
+}
